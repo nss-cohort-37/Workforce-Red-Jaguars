af9b4a26d799f3dd97dfa9d2cc6a347f4851d66e
rebeccapatek <rebeccapatek@gmail.com>
2020-04-15 14:19:16 -0500

    got index of Departments to work

---
+++ BangazonWorkforce/Controllers/DepartmentsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkforce.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id, d.[Name], d.Budget, COUNT (e.Id) as EmployeeCount FROM Department d
+                                        LEFT JOIN Employee e ON d.Id = e.DepartmentId
+                                        GROUP BY d.Id, d.[Name], d.Budget";
+
+                    var reader = cmd.ExecuteReader();
+                    var departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        var department = new Department()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                            EmployeeCount=reader.GetInt32(reader.GetOrdinal("EmployeeCount"))
+                           
+
+                        };
+                        departments.Add(department);
+                    }
+                    reader.Close();
+                    return View(departments);
+                }
+            }
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- BangazonWorkforce/Models/Department.cs
+++ BangazonWorkforce/Models/Department.cs
@@ -10,5 +10,7 @@ namespace BangazonWorkforce.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Budget { get; set; }
+
+        public int EmployeeCount { get; set; }
     }
 }
