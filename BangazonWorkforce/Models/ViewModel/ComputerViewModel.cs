a2b36b94b29242b94fdd62d42ab8852e788a8fdf
jansenv <jansenv44@gmail.com>
2020-04-16 08:39:26 -0500

    setting up view model

--- BangazonWorkforce/Models/ViewModel/ComputerViewModel.cs
+++ BangazonWorkforce/Models/ViewModel/ComputerViewModel.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BangazonWorkforce.Models
+namespace BangazonWorkforce.Models.ViewModel
 {
-    public class Computer
+    public class ComputerViewModel
     {
         public int Id { get; set; }
         public DateTime PurchaseDate { get; set; }
         public DateTime? DecomissionDate { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
-        public Employee Employee { get; set; }
+        public List<SelectListItem> EmployeeOptions { get; set; }
     }
 }
